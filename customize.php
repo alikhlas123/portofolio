aaafb3b10fe941624c8856998b9e1a0b91cadbbb
alikhlas123 <95178054+alikhlas123@users.noreply.github.com>
2023-11-07 15:40:01 +0700

    Add files via upload

---
+++ customize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://alikhlas123.github.io/portofolio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Fwp-admin%252Findex.php%26url%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Flogin-designer%252F%26simply_static_page%3D21090&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://alikhlas123.github.io/portofolio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Fwp-admin%252Findex.php%26url%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Flogin-designer%252F%26simply_static_page%3D21090&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://alikhlas123.github.io/portofolio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Fwp-admin%252Findex.php%26url%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Flogin-designer%252F%26simply_static_page%3D21090&reauth=1">https://alikhlas123.github.io/portofolio/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwordpress%2Fwp-admin%2Fcustomize.php%3Fautofocus%255Bsection%255D%3Dlogin_designer__section--templates%26return%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Fwp-admin%252Findex.php%26url%3Dhttp%253A%252F%252Flocalhost%252Fwordpress%252Flogin-designer%252F%26simply_static_page%3D21090&reauth=1</a></p>
+	</body>
+</html>
